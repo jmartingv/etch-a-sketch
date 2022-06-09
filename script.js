33629f42ab3a089d1f9a6a9d0f11711bdd83cfe2
Jesus <jmartingv96@gmail.com>
2022-06-08 22:33:55 -0700

    added buttons and functions to manipulate grid

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const container = document.querySelector('.grid-container');
+const sizeBtn = document.querySelector('#size');
+const clearBtn = document.querySelector('#clear');
 
-function setSize(size) {
+// Function for creating the grid
+function createGrid(size) {
+  container.innerHTML = '';
   for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
       let square = document.createElement('div');
+      square.classList.add('square');
 
       square.addEventListener('mouseover', (e) => {
         e.target.style.backgroundColor = 'red';
@@ -11,11 +16,40 @@ function setSize(size) {
 
       container.style.gridTemplateRows = `repeat(${size},1fr)`;
       container.style.gridTemplateColumns = `repeat(${size},1fr)`;
+
       container.appendChild(square);
     }
   }
 }
 
-setSize(100);
+// Clearing the grid
+function clearGrid() {
+  let squares = document.querySelectorAll('.square');
+  squares.forEach((square) => {
+    square.style.backgroundColor = '';
+  });
+}
+
+// Setting the size
+function setSize() {
+  let gridSize = Number(prompt('Set the size for the square grid (1 - 100): '));
+
+  while (gridSize > 100 || gridSize < 1 || !Number.isInteger(gridSize)) {
+    gridSize = Number(prompt('Please enter a valid value for the grid size: '));
+  }
+
+  return gridSize;
+}
+
+createGrid(16);
+
+sizeBtn.addEventListener('click', () => {
+  let size = setSize();
+  createGrid(size);
+});
+
+clearBtn.addEventListener('click', () => {
+  clearGrid();
+});
 
 console.log(container);
